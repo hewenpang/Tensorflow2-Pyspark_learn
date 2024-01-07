a3a27c9b2c07fd7425d2397fecec64390b1bccef
Ryan Pang <ryan0610@foxmail.com>
2024-01-07 11:04:06 +0800

    commit to local cache

---
+++ TF/tf_template.py
@@ -0,0 +1,27 @@
+# import matplotlib.pyplot as plt
+# history_dict = history.history
+# history_dict.keys()
+# acc = history_dict['accuracy']
+# val_acc = history_dict['val_accuracy']
+# loss = history_dict['loss']
+# val_loss = history_dict['val_loss']
+# epochs = range(1, len(acc)+1)
+
+# plt.plot(epochs, loss, 'bo', label='train loss')
+# plt.plot(epochs, val_loss, 'b', label='val loss')
+# plt.title('Train and val loss')
+# plt.xlabel('Epochs')
+# plt.xlabel('loss')
+# plt.legend()
+# plt.show()
+
+# plt.clf()   # clear figure
+
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.xlabel('Epochs')
+# plt.ylabel('Accuracy')
+# plt.legend()
+
+# plt.show()
